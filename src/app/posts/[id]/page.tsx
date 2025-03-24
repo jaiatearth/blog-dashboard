7825eb00b00f0dc8b4d543d8cea4ef29e7491384
jaiatearth <jayashree.sakthivel@gmail.com>
2025-03-24 23:21:34 +0530

    Add await to postID

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,31 +1,30 @@
-import { notFound } from "next/navigation";
-
 interface Post {
-  id: number;
-  title: string;
-  author: string;
-  body: string;
+    id: number;
+    title: string;
+    body: string;
 }
 
 async function getPost(id: string): Promise<Post | null> {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
-
-  // If ID not found..
-  if (!res.ok) return null;
-
-  return res.json();
+    const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${id}`);
+    if (!res.ok) return null;
+    return res.json();
 }
 
-export default async function PostDetail({ params }: { params: { id: string } }) {
-  const post = await getPost(params.id);
+export default async function PostDetail({
+    params,
+}: {
+    params: { id: string };
+}) {
+    const post = await getPost(params.id);
 
-  if (!post) return notFound();
+    if (!post) {
+        return <h1>Post Not Found</h1>;
+    }
 
-  return (
-    <main style={{ padding: "2rem" }}>
-      <h1>{post.title}</h1>
-      <p><strong>Author:</strong> {post.author || "Unknown"}</p>
-      <p>{post.body}</p>
-    </main>
-  );
+    return (
+        <main style={{ padding: "2rem" }}>
+            <h1>{post.title}</h1>
+            <p>{post.body}</p>
+        </main>
+    );
 }
